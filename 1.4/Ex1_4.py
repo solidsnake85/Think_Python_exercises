faa13cb1df38265f611203804f628b6c98715644
AnalogMaddog <matt.meyer@mac.com>
2022-11-16 18:06:42 -0800

    Exercise 1.4
    My answers to exercise 1.4

---
+++ 1.4/Ex1_4.py
@@ -0,0 +1,9 @@
+# Run a 10km race in 43 mins 30 secs / 1.  Avg time per mile?  2.  Average speed in mph?
+# 1.61 km in a mile
+
+miles = 1.61 * 10  # km to miles
+print ('miles = ' , miles)
+
+print (43.5 / miles)
+
+print ((miles / 43.5) * 60)
